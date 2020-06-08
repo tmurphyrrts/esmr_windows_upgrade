cb15c0197a278c4c9b98fe86538e9851b3b56b5a
tmurphyrrts <66630149+tmurphyrrts@users.noreply.github.com>
2020-06-08 15:02:42 -0500

    Update win10upgrade.ps1
    Added scheduled task for cleanup

--- win10upgrade.ps1
+++ win10upgrade.ps1
@@ -5,3 +5,9 @@ $url = 'https://go.microsoft.com/fwlink/?LinkID=799445'
 $file = "$dir\Win10Upgrade.exe"
 $webClient.DownloadFile($url,$file)
 Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /NoReboot /copylogs $dir /showoobe none'
+
+#Create scheduled task for cleanup
+$action = New-ScheduledTaskAction -Execute .\cleanup.ps1 
+$trigger = New-ScheduledTaskTrigger -Once -At 3am
+
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Cleanup Windows Upgrade Files" -Description "One time cleanup of the files that were downloaded to upgrade Windows 10"
