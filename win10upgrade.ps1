4cbf07dd27e437b89aa52918776f8f8ffa5aa559
tmurphyrrts <66630149+tmurphyrrts@users.noreply.github.com>
2020-06-08 15:14:45 -0500

    Update win10upgrade.ps1
    Updated scheduled task arguments

--- win10upgrade.ps1
+++ win10upgrade.ps1
@@ -7,7 +7,7 @@ $webClient.DownloadFile($url,$file)
 Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /NoReboot /copylogs $dir /showoobe none'
 
 #Create scheduled task for cleanup
-$action = New-ScheduledTaskAction -Execute .\cleanup.ps1 
+$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-File C:\Windows\Temp\Win10Upgrade\esmr_windows_upgrade-master\cleanup.ps1"
 $trigger = New-ScheduledTaskTrigger -Once -At 3am
 
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Cleanup Windows Upgrade Files" -Description "One time cleanup of the files that were downloaded to upgrade Windows 10 and Sets the Execution Policy back to Restricted"
